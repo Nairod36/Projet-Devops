8c6f9e8f7a11fa6f91e128f4d78df327339236d3
Nairod36 <moulin.solal@gmail.com>
2023-11-23 08:36:59 +0100

    ajt srv

---
+++ serverPullIMG/srv.tsx
@@ -0,0 +1,45 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { exec } from 'child_process';
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.post('/webhook', (req, res) => {
+  const event = req.headers['x-docker-hub-event'];
+  const repo = 'nairod36/zootest:main';
+
+  if (event === 'push') {
+    exec(`docker pull ${repo}`, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error: ${error.message}`);
+        return;
+      }
+      console.log(`Pulling image: ${repo}`);
+    });
+  }
+
+  res.status(200).end();
+});
+
+// Check for updates every 5 minutes 
+const updateInterval = 300000;
+
+setInterval(() => {
+  const repo = 'nairod36/zootest:main';
+
+  // Replace with the command to pull the updated image
+  exec(`docker pull ${repo}`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error: ${error.message}`);
+      return;
+    }
+    console.log('Periodic update check - Image pulled successfully.');
+  });
+}, updateInterval);
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
